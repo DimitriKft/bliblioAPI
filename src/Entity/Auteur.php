1cdac81b9b118fe598eb1c444917f0749fffbff2
klopfstein <dimitri.kft@gmail.com>
2020-09-09 15:36:26 +0200

    add crud api for auteur

--- src/Entity/Auteur.php
+++ src/Entity/Auteur.php
@@ -17,30 +17,32 @@ class Auteur
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"listAuteurFull", "listAuteurSimple"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"listGenreFull"})
+     * @Groups({"listAuteurFull", "listAuteurSimple", "listGenreFull"})
      */
     private $nom;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"listGenreFull"})
+     * @Groups({"listAuteurFull", "listAuteurSimple", "listGenreFull"})
      */
     private $prenom;
 
     /**
      * @ORM\ManyToOne(targetEntity=Nationalite::class, inversedBy="auteurs")
      * @ORM\JoinColumn(nullable=false)
-     * @Groups({"listGenreFull"})
+     * @Groups({"listAuteurFull", "listGenreFull", "listAuteurSimple"})
      */
     private $nationalité;
 
     /**
      * @ORM\OneToMany(targetEntity=Livre::class, mappedBy="auteur")
+     * @Groups({"listAuteurFull"})
      */
     private $livres;
 
